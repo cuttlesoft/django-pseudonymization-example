374bd55c262156f5f1fbc127fde3389e67287d00
Katie Russ <katie@cuttlesoft.com>
2018-06-11 18:03:22 -0400

    Add property fields to admin for users

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.forms import UserChangeForm as AuthUserChangeForm
@@ -5,11 +6,31 @@ from .models import User
 
 
 class UserChangeForm(AuthUserChangeForm):
-    pass
+    name = forms.CharField()
+    phone = forms.CharField()
+    date_of_birth = forms.DateField()
+    ip_address = forms.GenericIPAddressField()
+
+    class Meta:
+        model = User
+
+        fields = (
+            'username',
+            'password',
+            'name',
+            'phone',
+            'date_of_birth',
+            'ip_address',
+        )
 
 
 @admin.register(User)
 class UserAdmin(AuthUserAdmin):
     form = UserChangeForm
+
+    fieldsets = [
+        (None, {'fields': ['username', 'password']}),
+        ('Personal Data', {'fields': ['name', 'phone', 'date_of_birth', 'ip_address']}),
+    ]
 
     list_display = ('username', 'name', 'phone', 'date_of_birth', 'ip_address')
