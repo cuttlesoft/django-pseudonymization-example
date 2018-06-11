a882fe18c57c56a80d8d56fd02243eb71bfe4b2e
Katie Russ <katie@cuttlesoft.com>
2018-06-11 15:43:32 -0400

    Replace model field access with getters/setters

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
 
+from app.utils import mask, unmask
+
 
 class User(AbstractUser):
 
@@ -10,7 +12,39 @@ class User(AbstractUser):
         message="Phone number must have the format: '+9999999999'.",
     )
 
-    name = models.CharField(max_length=128, blank=True)
-    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
-    date_of_birth = models.DateField(blank=True, null=True)
-    ip_address = models.GenericIPAddressField(blank=True, null=True)
+    _name = models.CharField(max_length=128, blank=True)
+    _phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    _date_of_birth = models.DateField(blank=True, null=True)
+    _ip_address = models.GenericIPAddressField(blank=True, null=True)
+
+    @property
+    def name(self):
+        return unmask(self._name)
+
+    @name.setter
+    def name(self, value):
+        self._name = mask(value)
+
+    @property
+    def phone(self):
+        return unmask(self._phone)
+
+    @phone.setter
+    def phone(self, value):
+        self._phone = mask(value)
+
+    @property
+    def date_of_birth(self):
+        return unmask(self._date_of_birth)
+
+    @date_of_birth.setter
+    def date_of_birth(self, value):
+        self._date_of_birth = mask(value)
+
+    @property
+    def ip_address(self):
+        return unmask(self._ip_address)
+
+    @ip_address.setter
+    def ip_address(self, value):
+        self._ip_address = mask(value)
