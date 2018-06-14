9d8c7342aaa49af1c973fe223c48a6d415286795
Katie Russ <katie.k7r@gmail.com>
2018-06-14 12:25:38 -0400

    Customize fields in user admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,39 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from django.contrib.auth.forms import UserChangeForm as AuthUserChangeForm
+from .models import User
 
-# Register your models here.
+
+class UserChangeForm(AuthUserChangeForm):
+    name = forms.CharField(max_length=128)
+    phone = forms.CharField(max_length=16)
+    date_of_birth = forms.DateField()
+    ip_address = forms.GenericIPAddressField()
+
+    def __init__(self, *args, **kwargs):
+        super(UserChangeForm, self).__init__(*args, **kwargs)
+        self.fields['date_of_birth'].widget = admin.widgets.AdminDateWidget()
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'password',
+            'name',
+            'phone',
+            'date_of_birth',
+            'ip_address',
+        )
+
+
+@admin.register(User)
+class UserAdmin(AuthUserAdmin):
+    form = UserChangeForm
+
+    list_display = ('username', 'name', 'phone', 'date_of_birth', 'ip_address')
+
+    fieldsets = [
+        (None, {'fields': ['username', 'password']}),
+        ('Personal Data', {'fields': ['name', 'phone', 'date_of_birth', 'ip_address']}),
+    ]
